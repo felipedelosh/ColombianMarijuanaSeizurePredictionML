e94755a4e06fea6bbf519fd82746913574138c03
felipedelosh <doctorfhernandez@hotmail.com>
2024-11-09 14:40:44 -0500

    Update Statitics

--- UTIL.STRACT.MUNICIPIO.py
+++ UTIL.STRACT.MUNICIPIO.py
@@ -8,6 +8,7 @@ _places_codes = {}
 with open("INCAUTACIONES_DE_MARIHUANA_20241108.csv", "r", encoding="UTF-8") as f:
     _data = f.read()
 
+# Only unique code values save
 for i in _data.split("\n"):
     if str(i).strip() != "":
         _d = str(i).split(",")
@@ -19,4 +20,20 @@ for i in _data.split("\n"):
             _places_codes[_name] = _cod
 
 
-print(_places_codes)
+# Prepare to save in municipios.txt
+_output = ""
+_counter = 0
+for i in _places_codes:
+    # Only valid places codes: int
+    try:
+        _int = int(_places_codes[i])
+        _counter = _counter + 1
+        _output = _output + f"{i}:{_places_codes[i]}\n"
+    except:
+        pass
+
+
+_output = f"TOTAL: {_counter}\n" + _output
+
+with open("municipios.txt", "w", encoding="UTF-8") as f:
+    f.write(_output)
